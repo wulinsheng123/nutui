7702e0e40af335e8dec63f9e3c8dc13c5d3e8af6
richard1015 <51844712@qq.com>
2020-09-11 20:14:34 +0800

    upd: init nutui-taro

---
+++ src/packages/button/button.vue
@@ -0,0 +1,17 @@
+<template>
+  <view class="button">
+    <text>{{ msg }}</text>
+  </view>
+</template>
+
+<script>
+import './button.scss'
+
+export default {
+  data () {
+    return {
+      msg: 'Hello world!'
+    }
+  }
+}
+</script>
