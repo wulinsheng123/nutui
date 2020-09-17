910e4cbc80eb48def335bc6e6caf364965cf23c6
richard1015 <51844712@qq.com>
2020-09-17 22:01:27 +0800

    upd: nutui-taro test

--- src/packages/button/button.vue
+++ src/packages/button/button.vue
@@ -1,17 +1,70 @@
 <template>
-  <view class="button">
-    <text>{{ msg }}</text>
-  </view>
+  <button :class="clsStyle" :disabled="disabled" @click="clickHandler">
+    <slot></slot>
+  </button>
 </template>
 
 <script>
-import './button.scss'
-
+import './button.scss';
 export default {
-  data () {
+  name: 'nut-button',
+  props: {
+    type: {
+      type: String,
+      default: '',
+    },
+    shape: {
+      type: String,
+      default: '',
+    },
+    icon: {
+      type: String,
+      default: '',
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    block: {
+      type: Boolean,
+      default: false,
+    },
+    size: {
+      type: String,
+      default: 'big',
+    },
+    label: {
+      type: Boolean,
+      default: false,
+    },
+    color: {
+      type: String,
+      default: '',
+    },
+  },
+  data() {
     return {
-      msg: 'Hello world!'
-    }
-  }
-}
+      msg: 'Hello world!',
+    };
+  },
+  computed: {
+    clsStyle() {
+      let cls = `nut-button ${this.type} ${this.shape} 
+            ${this.size} 
+            ${this.block ? ' block' : ''} 
+            ${this.label ? ' label' : ''}
+            ${!this.$slots.default ? `no-txt-${this.size}` : ''}`;
+      return cls;
+    },
+  },
+  methods: {
+    clickHandler(event) {
+      // 如果是loading就阻止点击
+      if (this.disabled) {
+        return;
+      }
+      this.$emit('click', event);
+    },
+  },
+};
 </script>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,17 +1,82 @@
 <template>
-  <view class="index">
-    <text>{{ msg }}</text>
+  <view class="nutui-taro-demo">
+    <text class="title">{{name}}</text>
+    <text class="desc">{{description}}</text>
+    <view class="comp-list">
+      <text class="comp-text">Button</text>
+      <nut-button>button</nut-button>
+    </view>
+    <text class="footer">京东零售用户体验设计部 · 前端开发部 · v{{ version }}</text>
   </view>
+
 </template>
 
 <script>
-import './index.scss'
-
+import { version, name, description } from '../../../package.json';
+import Button from '../../packages/button/button.vue';
+import '../../packages/button/button.scss';
 export default {
-  data () {
+  components:{
+    'nut-button':Button
+  },
+  data() {
     return {
-      msg: 'Hello world!'
+      msg: 'Hello world!',
+      description,
+      name,
+      version,
+    };
+  },
+};
+</script>
+
+
+<style lang="scss">
+.nutui-taro-demo {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  top: 0;
+  right: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  box-sizing: border-box;
+  background: #f7f7f7;
+
+  .title {
+    font-size: 48px;
+    color: #2e2d2d;
+    margin-top: 200px;
+  }
+  .desc {
+    text-align: center;
+    color: #848484;
+    margin: 40px 0 60px;
+    font-size: 24px;
+  }
+
+  .comp-list {
+    margin: 30px auto;
+    width: 95%;
+    background: #fff;
+    -webkit-border-radius: 30px;
+    border-radius: 30px;
+    overflow: hidden;
+
+    padding: 0 30px;
+
+    .comp-text {
+      font-size: 32px;
     }
   }
+
+  .footer {
+    font-size: 20px;
+    color: #bbb;
+    text-shadow: 1px 1px #fff;
+    text-align: center;
+    margin: 20px 0;
+  }
 }
-</script>
+</style>
